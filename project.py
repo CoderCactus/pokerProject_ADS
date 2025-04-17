de8ab095f21a291e611480f8e4df538c426a57b2
slenderstream90 <97785517+slenderstream90@users.noreply.github.com>
2025-04-17 08:35:26 +0000

    Implemented Shuffle function

--- project.py
+++ project.py
@@ -8,26 +8,30 @@ class Card:
 class Deck:
     def __init__(self):
         self.cards = []
-        for i in [1,2,3,4,5,6,7,8,9,10,'J','Q','K']:
+        for i in ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']:
             for j in ['clubs', 'hearts', 'diamonds', 'spades']:
                 self.cards.append(Card(i, j))
+
+    def shuffle(self):
+        for i in range(len(self.cards)):
+            randSeed = randint(i, len(self.cards)-1)
+            self.cards[i], self.cards[randSeed] = swap(self.cards[i], self.cards[randSeed])
         
     def drawing(self, card_number):
-        drawn = []
-    
-        for i in range(card_number):
-            randSeed = randint(i, len(self.cards))
-            drawn.append(self.cards[randSeed])
-            self.cards.pop(randSeed)
+        drawn = self.cards[:card_number]
+        self.cards = self.cards[card_number:]
         
         return(drawn)
+    
+def swap(n, m):
+    return m, n
 
 deck = Deck()
 
-# Draw 5 cards
+deck.shuffle()
+
 hand = deck.drawing(5)
 
-# Print drawn cards
 for card in hand:
     print(card.value, card.suit)
 
