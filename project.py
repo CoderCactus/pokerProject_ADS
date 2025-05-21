c9181afb256a1bf897f4ff7ba7960da7204e7843
slenderstream90 <97785517+slenderstream90@users.noreply.github.com>
2025-05-21 09:18:27 +0000

    Merge mergeSort and BinarySort

--- project.py
+++ project.py
@@ -57,14 +57,54 @@ class Hand:
             self.cards[0], self.cards[i] = self.cards[i], self.cards[0]  
             heapify(self.cards, i, 0)
      
-    def binarySort(self):
-        return
+    def binarySort(self, cards=None):
+        if cards is None:
+            cards = self.cards
+        for i in range(1, len(cards)):
+            current = cards[i]
+            left, right = 0, i
+            while left < right:
+                mid = (left + right) // 2
+                if current.numericValue < cards[mid].numericValue:
+                    right = mid
+                else:
+                    left = mid + 1
+            for j in range(i, left, -1):
+                cards[j] = cards[j - 1]
+            cards[left] = current
+        return cards
+    
+    
     def discard(self):
         self.cards = []
 
-    def mergeSort(self):
-        #TODO
-        return
+
+    def mergeSort(self, cards=None):
+        if cards is None:
+            cards = self.cards
+
+        if len(cards) <= 1:
+            return cards
+
+        mid = len(cards) // 2
+        left = self.mergeSort(cards[:mid])
+        right = self.mergeSort(cards[mid:])
+        return self.merge(left, right)
+
+    def merge(self, left, right):
+        result = []
+        i = j = 0
+        while i < len(left) and j < len(right):
+            if left[i].numericValue <= right[j].numericValue:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
+    
     def insertionSort(self):
         for i in range(1, len(self.cards)):
             current_card = self.cards[i]
